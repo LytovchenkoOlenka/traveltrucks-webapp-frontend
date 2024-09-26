49897e7d7cb3eb7bee0f950472902ed41ca075c1
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-26 15:15:33 +0200

    styled_HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
+import { NavLink } from "react-router-dom";
 import css from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
     <>
-      <div className={css.generalContainer}>
-        <h2>Home Page</h2>
+      <div className={css.container}>
+        <div className={css.contentContainer}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.description}>
+            You can find everything you want in our catalog
+          </p>
+          <NavLink className={css.button} to={"/catalog"}>
+            View Now
+          </NavLink>
+        </div>
       </div>
     </>
   );
