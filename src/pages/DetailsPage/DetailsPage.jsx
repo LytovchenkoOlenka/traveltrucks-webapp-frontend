8b941503c7a49d259236b871edb07bcca9151812
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-27 22:10:53 +0200

    Updated_DetailsPage

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,10 +1,25 @@
+import { NavLink } from "react-router-dom";
+import CamperMainDetails from "../../components/CamperMainDetails/CamperMainDetails";
 import css from "./DetailsPage.module.css";
+import clsx from "clsx";
+
+const getNavLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 export default function DetailsPage() {
   return (
     <>
-      <section className={css.generalContainer}>
-        <h2>DetailsPage</h2>
+      <section className={css.pageContainer}>
+        <CamperMainDetails />
+        <nav className={css.navLink}>
+          <NavLink className={getNavLinkClass} to="features">
+            Features
+          </NavLink>
+          <NavLink className={getNavLinkClass} to="reviews">
+            Review
+          </NavLink>
+        </nav>
       </section>
     </>
   );
