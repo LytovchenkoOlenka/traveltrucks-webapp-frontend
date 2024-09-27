49126f3f392ef2f86dfeb5523f657b357848d349
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-27 22:11:56 +0200

    Changed_routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
 const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage.jsx"));
 const DetailsPage = lazy(() => import("./pages/DetailsPage/DetailsPage.jsx"));
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
+const Features = lazy(() => import("./components/Features/Features.jsx"));
+const Reviews = lazy(() => import("./components/Reviews/Reviews.jsx"));
 import Layout from "./components/Layout/Layout.jsx";
 
 export default function App() {
@@ -15,7 +17,10 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:id" element={<DetailsPage />} />
+          <Route path="/catalog/:id" element={<DetailsPage />}>
+            <Route path="features" element={<Features />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Layout>
