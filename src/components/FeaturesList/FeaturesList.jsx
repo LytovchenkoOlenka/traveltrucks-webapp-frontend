1e1756de375a41b4531a70d0534d2c3d81f6368e
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-29 18:00:10 +0200

    Update_featuresList

--- src/components/FeaturesList/FeaturesList.jsx
+++ src/components/FeaturesList/FeaturesList.jsx
@@ -1,67 +1,40 @@
 import css from "./FeaturesList.module.css";
 
-const data = {
-  name: "Road Bear C 23-25",
-  price: 10000,
-  rating: 4.5,
-  location: "Ukraine, Kyiv",
-  description:
-    "Embadventures, promising comfort, style, and the freedom to explore at your own pace.",
-  form: "alcove",
-  length: "7.3m",
-  width: "2.65m",
-  height: "3.65m",
-  tank: "208l",
-  consumption: "30l/100km",
-  transmission: "automatic",
-  engine: "diesel",
-  AC: true,
-  bathroom: true,
-  kitchen: false,
-  TV: true,
-  radio: true,
-  refrigerator: false,
-  microwave: true,
-  gas: false,
-  water: true,
-};
+export default function FeaturesList({ data }) {
+  const {
+    AC,
+    bathroom,
+    kitchen,
+    TV,
+    radio,
+    refrigerator,
+    microwave,
+    gas,
+    water,
+  } = data;
 
-const {
-  AC,
-  bathroom,
-  kitchen,
-  TV,
-  radio,
-  refrigerator,
-  microwave,
-  gas,
-  water,
-} = data;
+  const features = {
+    AC,
+    bathroom,
+    kitchen,
+    TV,
+    radio,
+    refrigerator,
+    microwave,
+    gas,
+    water,
+  };
 
-const features = {
-  AC,
-  bathroom,
-  kitchen,
-  TV,
-  radio,
-  refrigerator,
-  microwave,
-  gas,
-  water,
-};
-
-const filtredFeatures = Object.keys(features).filter((key) => features[key]);
-
-console.log(filtredFeatures);
-
-export default function FeaturesList() {
-  <>
-    <ul className={css.featuresList}>
-      {filtredFeatures.map((feature) => (
-        <li key={feature} className={css.featuresItem}>
-          <p className={css.featureText}>{feature}</p>
-        </li>
-      ))}
-    </ul>
-  </>;
+  const filtredFeatures = Object.keys(features).filter((key) => features[key]);
+  return (
+    <>
+      <ul className={css.featuresList}>
+        {filtredFeatures.map((feature) => (
+          <li key={feature} className={css.featuresItem}>
+            <p className={css.featureText}>{feature}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
