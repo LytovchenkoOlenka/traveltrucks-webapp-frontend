378feb71d28ac49eb2aeeedee97bc88528fd8610
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-29 17:59:38 +0200

    Set_CamperList_&_CamperCard

--- src/components/CamperCard/CamperCard.jsx
+++ src/components/CamperCard/CamperCard.jsx
@@ -3,64 +3,41 @@ import { IoMdHeartEmpty } from "react-icons/io";
 // import { MdEuroSymbol } from "react-icons/md";
 import { SlMap } from "react-icons/sl";
 import { FaStar } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import FeaturesList from "../FeaturesList/FeaturesList";
+// import { Link } from "react-router-dom";
 
-const data = {
-  name: "Road Bear C 23-25",
-  price: 10000,
-  rating: 4.5,
-  location: "Ukraine, Kyiv",
-  description:
-    "Embadventures, promising comfort, style, and the freedom to explore at your own pace.",
-  form: "alcove",
-  length: "7.3m",
-  width: "2.65m",
-  height: "3.65m",
-  tank: "208l",
-  consumption: "30l/100km",
-  transmission: "automatic",
-  engine: "diesel",
-  AC: true,
-  bathroom: true,
-  kitchen: false,
-  TV: true,
-  radio: true,
-  refrigerator: false,
-  microwave: true,
-  gas: false,
-  water: true,
-};
+export default function CamperCard({ data }) {
+  const image = data.gallery[0];
 
-const {
-  AC,
-  bathroom,
-  kitchen,
-  TV,
-  radio,
-  refrigerator,
-  microwave,
-  gas,
-  water,
-} = data;
+  // const {
+  //   AC,
+  //   bathroom,
+  //   kitchen,
+  //   TV,
+  //   radio,
+  //   refrigerator,
+  //   microwave,
+  //   gas,
+  //   water,
+  // } = data;
 
-const features = {
-  AC,
-  bathroom,
-  kitchen,
-  TV,
-  radio,
-  refrigerator,
-  microwave,
-  gas,
-  water,
-};
+  // const features = {
+  //   AC,
+  //   bathroom,
+  //   kitchen,
+  //   TV,
+  //   radio,
+  //   refrigerator,
+  //   microwave,
+  //   gas,
+  //   water,
+  // };
 
-const filtredFeatures = Object.keys(features).filter((key) => features[key]);
+  // const filtredFeatures = Object.keys(features).filter((key) => features[key]);
 
-export default function CamperCard() {
   return (
     <div className={css.containerCard}>
-      <img src="/Pic.jpg" alt="" />
+      <img className={css.mainImage} src={image.thumb} alt="" />
 
       <div className={css.camperInfo}>
         <div>
@@ -68,7 +45,13 @@ export default function CamperCard() {
             <h2 className={css.title}>{data.name}</h2>
             <span className={css.priceAndFavourite}>
               {/* <MdEuroSymbol /> */}
-              <p>€{data.price}</p>
+              <p>
+                €
+                {data.price.toLocaleString("uk-UA", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </p>
               <IoMdHeartEmpty className={css.heart} />
             </span>
           </div>
@@ -87,17 +70,23 @@ export default function CamperCard() {
 
         <p className={css.description}>{data.description}</p>
 
-        <ul className={css.featuresList}>
+        <FeaturesList data={data} />
+        {/* <ul className={css.featuresList}>
           {filtredFeatures.map((feature) => (
             <li key={feature} className={css.featuresItem}>
               <p className={css.featureText}>{feature}</p>
             </li>
           ))}
-        </ul>
+        </ul> */}
 
-        <Link className="button" to={`/catalog/{data.id}`}>
+        <a
+          className="button"
+          href={`/catalog/${data.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Show More
-        </Link>
+        </a>
       </div>
     </div>
   );
--- src/components/CampersList/CampersList.jsx
+++ src/components/CampersList/CampersList.jsx
@@ -1,33 +1,26 @@
-// import { Link } from "react-router-dom";
 import css from "./CampersList.module.css";
 import CamperCard from "../CamperCard/CamperCard";
 
+import { useSelector } from "react-redux";
+import { selectCampers } from "../../redux/filters/selectors";
+
 export default function CampersList() {
+  const { items } = useSelector(selectCampers);
+
   return (
-    <div>
-      <div className={css.containerCatalog}>
-        <CamperCard />
-        <CamperCard />
-        <CamperCard />
-        <CamperCard />
-      </div>
+    <section className={css.containerCatalog}>
+      <ul className={css.campersList}>
+        {items.length !== 0 ? (
+          items.map((camper) => (
+            <li className={css.item} key={camper.id}>
+              <CamperCard data={camper} />
+            </li>
+          ))
+        ) : (
+          <p>No campers found</p>
+        )}
+      </ul>
       <button className={css.button}>Load more</button>
-    </div>
+    </section>
   );
 }
-
-// export default function CampersList({ campers }) {
-//   return (
-//     <div className={css.containerCatalog}>
-//       <ul className={css.list}>
-//         {campers.map((camper) => (
-//           <li className={css.item} key={camper.id}>
-//             <Link className={css.cardLink} to={`/catalog/${camper.id}`}>
-//               <CamperCard data={camper}/>
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
