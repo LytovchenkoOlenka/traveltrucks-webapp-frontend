f3abe80a5f0eb025924a93b5232c82722176c0ce
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-30 22:32:30 +0200

    Revert "added_petsistGate(commented)"
    This reverts commit 13117c4ad1bb19d57fca6ca4f18f68dc5a461cff.

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,13 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-// import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   </StrictMode>
 );
