6ef26eab48d0f8ab4593730decf501d82a6eee2b
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-30 18:35:22 +0200

    deleted_some_old_code

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
 const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage.jsx"));
 const DetailsPage = lazy(() => import("./pages/DetailsPage/DetailsPage.jsx"));
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
-const Features = lazy(() => import("./components/Features/Features.jsx"));
-const Reviews = lazy(() => import("./components/Reviews/Reviews.jsx"));
+
 import Layout from "./components/Layout/Layout.jsx";
 
 export default function App() {
@@ -18,10 +17,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:id" element={<DetailsPage />}>
-            <Route path="features" element={<Features />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
+          <Route path="/catalog/:id" element={<DetailsPage />}></Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Layout>
