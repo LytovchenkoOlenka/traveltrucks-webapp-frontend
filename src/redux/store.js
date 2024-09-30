13117c4ad1bb19d57fca6ca4f18f68dc5a461cff
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-30 22:29:35 +0200

    added_petsistGate(commented)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 import campersReducer from "./campers/slice";
 import filtersReducer from "./filters/slice";
 import favoritesReducer from "./favorites/slice";
+
+// const favoritesPersistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["favorites"],
+// };
+
+// const pFavotiresReducer = persistReducer(
+//   favoritesPersistConfig,
+//   favoritesReducer
+// );
 
 export const store = configureStore({
   reducer: {
@@ -9,4 +31,12 @@ export const store = configureStore({
     filters: filtersReducer,
     favorites: favoritesReducer,
   },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
+
+// export const persistor = persistStore(store);
