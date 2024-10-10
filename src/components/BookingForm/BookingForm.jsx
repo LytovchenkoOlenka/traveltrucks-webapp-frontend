11dcb5295ec34bffa8ffb38b80c37f96596c8e14
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-10-10 20:28:18 +0200

    added_ErrorMessage_to_bookingForm

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import css from "./BookingForm.module.css";
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import toast from "react-hot-toast";
 
@@ -17,6 +17,7 @@ const FeedbackSchema = Yup.object().shape({
     .required("Email is required")
     .email("Please enter a valid email address"),
   date: Yup.date()
+    .typeError("Invalid date format")
     .required("Booking date is required")
     .min(new Date(), "Only future dates!"),
   comment: Yup.string().trim().max(500, "Comment can't exceed 500 characters"),
@@ -48,38 +49,54 @@ export default function BookingForm() {
       </p>
 
       <Formik
-        onSubmit={handleSubmit}
         initialValues={initialValues}
         validationSchema={FeedbackSchema}
+        onSubmit={handleSubmit}
       >
         <Form className={css.form} autoComplete="off">
-          <Field
-            className={css.input}
-            type="text"
-            placeholder="Name*"
-            name="name"
-            required
-          />
-          <Field
-            className={css.input}
-            type="email"
-            placeholder="Email*"
-            name="email"
-            required
-          />
-          <Field
-            className={css.input}
-            component={DatePickerField}
-            placeholder="Booking date*"
-            name="date"
-          />
-          <Field
-            as="textarea"
-            name="comment"
-            className={clsx(css.input, css.textArea)}
-            placeholder="Comment"
-            rows={3}
-          />
+          <div className={css.fieldWraper}>
+            <Field
+              className={css.input}
+              type="text"
+              placeholder="Name*"
+              name="name"
+              required
+            />
+            <ErrorMessage name="name" component="div" className={css.error} />
+          </div>
+          <div className={css.fieldWraper}>
+            <Field
+              className={css.input}
+              type="email"
+              placeholder="Email*"
+              name="email"
+              required
+            />
+            <ErrorMessage name="email" component="div" className={css.error} />
+          </div>
+          <div className={css.fieldWraper}>
+            <Field
+              className={css.input}
+              component={DatePickerField}
+              placeholder="Booking date*"
+              name="date"
+            />
+            <ErrorMessage name="date" component="div" className={css.error} />
+          </div>
+          <div className={css.fieldWraper}>
+            <Field
+              as="textarea"
+              name="comment"
+              className={clsx(css.input, css.textArea)}
+              placeholder="Comment"
+              rows={3}
+            />
+            <ErrorMessage
+              name="comment"
+              component="div"
+              className={css.error}
+            />
+          </div>
           <button className="button" type="submit">
             Send
           </button>
