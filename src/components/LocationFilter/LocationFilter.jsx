de9e81216d1fb5a1b54f6ca1c2df5b6cabc24d78
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-10-30 21:25:08 +0100

    fixed_location_filter

--- src/components/LocationFilter/LocationFilter.jsx
+++ src/components/LocationFilter/LocationFilter.jsx
@@ -1,14 +1,7 @@
 import css from "./LocationFilter.module.css";
 import { SlMap } from "react-icons/sl";
 
-// import { useDispatch, useSelector } from "react-redux";
-// import { changeLocation } from "../../redux/filters/slice";
-// import { selectFilters } from "../../redux/filters/selectors";
-
 export default function LocationFilter({ onLocationChange, location }) {
-  // const dispatch = useDispatch();
-  // const { location } = useSelector(selectFilters);
-
   const handleLocationChange = (event) => {
     onLocationChange(event.target.value);
   };
@@ -21,7 +14,6 @@ export default function LocationFilter({ onLocationChange, location }) {
           className={css.locationInput}
           type="text"
           value={location}
-          defaultValue={location}
           onChange={handleLocationChange}
           placeholder="Kyiv, Ukraine"
         />
